ddb2b626e80dfa5ac33a91a54c8bcdc9d52b2b8f
Lior Shaya <lior160500@gmail.com>
2024-12-10 15:34:17 +0200

    regular

--- src/termA/lesson6/Array2.java
+++ src/termA/lesson6/Array2.java
@@ -76,5 +76,70 @@ class Ex2{
 }
 
 class Ex3{
+    public static void main(String[] args) {
+        int[] array = {1,3,5,6,7,8,1,0};
+        int peakIndex = arrayUpDown(array);
+        System.out.println(peakIndex);
+    }
 
+    public static int arrayUpDown(int[] array){
+
+        int peakIndex = -1;
+
+        if(array.length <3){
+            return -1;
+        }
+
+        for (int i = 1; i < array.length-1; i++) { //locatePeak
+            if(array[i] > array[i-1] && array[i] > array[i+1]){
+                peakIndex = i;
+                break;
+            }
+        }
+
+        for (int i = 0; i < peakIndex; i++) { //locateUpBeforePeak
+            if(array[i] >= array[i+1]){
+                return -1;
+            }
+        }
+
+        for (int i = peakIndex; i < array.length-1; i++) {//locateDownAfterPeak
+            if(array[i] <= array[i+1]){
+                return -1;
+            }
+        }
+        return peakIndex;
+    }
+}
+
+class Ex4{
+    public static void main(String[] args) {
+       char[] board = new char[9] ;
+       char[] testBoard = {'X', 'O', 'X', ' ', 'O', ' ', 'X', ' ', 'O'};
+        printBoard(testBoard);
+    }
+
+    public static void printBoard(char[] board){
+        for (int i = 0; i < board.length; i++) {
+            System.out.print(board[i] + " ");
+
+            if((i +1) % 3 ==0){
+                System.out.println();
+            }
+
+        }
+    }
+
+    public static boolean isAvailable( char[] board , int indexNumber){
+        char x = 'X';
+        char o = 'O';
+
+        if(indexNumber < 0 || indexNumber >= board.length ){
+            return false;
+        }
+        if(board[indexNumber] == o || board[indexNumber] == x){
+            return false;
+        }
+    return true;
+    }
 }
