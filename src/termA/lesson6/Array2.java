8a54fb66a9e81aeabdc31408ae9784f67435b452
Lior Shaya <lior160500@gmail.com>
2024-12-10 18:22:34 +0200

    regular

--- src/termA/lesson6/Array2.java
+++ src/termA/lesson6/Array2.java
@@ -1,5 +1,7 @@
 package termA.lesson6;
 
+import java.util.Scanner;
+
 public class Array2 {
     //Ex1
     public static void main(String[] args) {
@@ -115,8 +117,52 @@ class Ex3{
 class Ex4{
     public static void main(String[] args) {
        char[] board = new char[9] ;
-       char[] testBoard = {'X', 'O', 'X', ' ', 'O', ' ', 'X', ' ', 'O'};
-        printBoard(testBoard);
+       char[] player= {'X','O'};
+       char currentPlayer;
+       boolean isWinner = false;
+
+        System.out.println("The format is:");
+        System.out.println("1 2 3");
+        System.out.println("4 5 6");
+        System.out.println("7 8 9");
+
+
+        for (int i = 0; i < 9; i++) {
+            if (i % 2 == 0) {
+                currentPlayer = player[0];
+            } else {
+                currentPlayer = player[1];
+            }
+
+            int position = getPositionFromUser(board);
+            isWinner = placeSymbolOnBoard(board, position, currentPlayer);
+
+            if(isWinner) {
+                break;
+            }
+
+        }
+
+        if(!isWinner){
+            System.out.println("Tie , Game is Over.");
+        }
+
+    }
+
+    public static void printBoardd(char[] board){
+        System.out.println("┏━━━┳━━━┳━━━┓");
+        for (int i = 0; i < board.length; i++) {
+            System.out.print("┃ " + (board[i] == '\u0000' ? ' ' : board[i]) + " ");
+
+            if (i % 3 == 2) {
+                System.out.println("┃");
+
+                if (i != board.length - 1) {
+                    System.out.println("┣━━━╋━━━╋━━━┫");
+                }
+            }
+        }
+        System.out.println("┗━━━┻━━━┻━━━┛");
     }
 
     public static void printBoard(char[] board){
@@ -134,12 +180,67 @@ class Ex4{
         char x = 'X';
         char o = 'O';
 
-        if(indexNumber < 0 || indexNumber >= board.length ){
+        if(board[indexNumber-1] == o || board[indexNumber-1] == x){
             return false;
         }
-        if(board[indexNumber] == o || board[indexNumber] == x){
-            return false;
-        }
-    return true;
+        return true;
     }
+
+    public static int getPositionFromUser(char[] board){
+        Scanner scanner = new Scanner(System.in);
+
+        int position;
+
+        while(true){
+            System.out.println("Please enter the position you would like to enter your symbol: ");
+
+            position = scanner.nextInt();
+
+            if(position <1 || position > 9){
+                System.out.println("Invalid position, please try again- this number out of range.");
+                continue;
+            }
+
+            if(isAvailable(board, position)){
+                return position;
+            }
+            else{
+                System.out.println("Position already taken, please choose another position.");
+            }
+
+        }
+    }
+
+    public static char checkWinner(char[] board){
+        for (int i = 0; i < 9; i+=3) {
+            if(board[i] != ' ' && board[i] == board[i+1] && board[i] == board[i+2]){
+                return board[i];
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (board[i] != ' ' && board[i] == board[i+3] && board[i] == board[i+6]){
+                return board[i];
+            }
+        }
+        if(board[0] != ' ' && board[0] == board[4] && board[0] == board[8]){
+            return board[0];
+        }
+        if(board[2] != ' ' && board[2] == board[4] && board[2] == board[6]){
+            return board[2];
+        }
+        return '-';
+    }
+
+    public static boolean placeSymbolOnBoard(char[] board, int position , char currentPlayer){
+        board[position-1] = currentPlayer;
+        printBoardd(board);
+        char winner = checkWinner(board);
+        if(winner == 'X' || winner == 'O'){
+            System.out.println("YooHoo " + winner + " is the winner!");
+            return true;
+        }
+        return false;
+    }
+
 }
