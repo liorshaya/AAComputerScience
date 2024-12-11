c536f3546efa87711963348e33cd31223dad8e12
Lior Shaya <lior160500@gmail.com>
2024-12-11 21:29:42 +0200

    regular

--- src/termA/lesson7/StringsLecture.java
+++ src/termA/lesson7/StringsLecture.java
@@ -1,5 +1,6 @@
 package termA.lesson7;
 
+//Ex1
 public class StringsLecture {
 
     public static void main(String[] args) {
@@ -23,6 +24,24 @@ public class StringsLecture {
     }
 }
 
+class Ex2{
+    public static void main(String[] args) {
+    char[] array = {'l','i','o','r'};
+    boolean result = contains(array, 'r');
+    System.out.println(result);
+    }
+
+    public static boolean contains(char[] arr , char toSearch){
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] == toSearch){
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
+
 class Ex3{
     public static void main(String[] args) {
         char[] arr = {'1','2','3','4'};
@@ -38,6 +57,46 @@ class Ex3{
             }
         }
         return -1;
+    }
+}
+
+class Ex4{
+    public static void main(String[] args) {
+        char[] array = {'l','i','o','r','o'};
+        boolean result = hasDuplicates(array);
+        System.out.println(result);
+    }
+    public static boolean hasDuplicates(char[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if(i != j && arr[i] == arr[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+class Ex5{
+    public static void main(String[] args) {
+        char[] array = {'M','A','M','A'};
+        char[] result = replace(array,'M','P');
+        System.out.println(result);
+    }
+    public static char[] replace(char[] arr , char original , char replacement){
+        char[] newArray = new char[arr.length];
+
+
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] == original) {
+                newArray[i] = replacement;
+            }
+            else{
+                newArray[i] = arr[i];
+            }
+        }
+        return newArray;
     }
 }
 
