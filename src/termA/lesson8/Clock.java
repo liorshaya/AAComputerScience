4dd9d421ece5aaa7f5403af14e1fa0d8cb1d51d9
SHAYA1605 <84973877+SHAYA1605@users.noreply.github.com>
2024-12-24 03:07:19 +0200

    regular

--- src/termA/lesson8/Clock.java
+++ src/termA/lesson8/Clock.java
@@ -1,6 +1,5 @@
 package termA.lesson8;
 
-import java.sql.SQLOutput;
 import java.util.Random;
 
 public class Clock {
@@ -9,35 +8,34 @@ public class Clock {
     int minutes;
     int hours;
 
-    void print(){
-        if(this.hours < 10){
+    void print() {
+        if (this.hours < 10) {
             System.out.print(0);
         }
         System.out.print(this.hours + ":");
-        if(this.minutes < 10){
+        if (this.minutes < 10) {
             System.out.print(0);
         }
         System.out.print(this.minutes + ":");
-        if(this.seconds < 10){
+        if (this.seconds < 10) {
             System.out.print(0);
         }
         System.out.print(this.seconds);
     }
 
-    void printAmPm(){
+    void printAmPm() {
         int original = this.hours;
         int pmHours = this.hours % 12;
 
 
-        if(pmHours == 0){
-            pmHours =12;
+        if (pmHours == 0) {
+            pmHours = 12;
         }
-        if(original >= 12){
+        if (original >= 12) {
             this.hours = pmHours;
             print();
             System.out.print(" PM");
-        }
-        else{
+        } else {
             print();
             System.out.print(" AM");
         }
@@ -46,10 +44,10 @@ public class Clock {
 
     }
 
-    boolean isValid(){
-        if(this.hours >= 0 && this.hours < 24){
-            if(this.minutes >= 0 && this.minutes <= 59){
-                if(this.seconds >= 0 && this.seconds <= 59){
+    boolean isValid() {
+        if (this.hours >= 0 && this.hours < 24) {
+            if (this.minutes >= 0 && this.minutes <= 59) {
+                if (this.seconds >= 0 && this.seconds <= 59) {
                     return true;
                 }
             }
@@ -57,7 +55,7 @@ public class Clock {
         return false;
     }
 
-    void randomize(){
+    void randomize() {
         Random random = new Random();
 
         this.hours = random.nextInt(24);
@@ -68,15 +66,15 @@ public class Clock {
 
     }
 
-    boolean isMorning(){
-        if(this.hours >= 8 && this.hours <= 11 ){
+    boolean isMorning() {
+        if (this.hours >= 8 && this.hours <= 11) {
             return true;
         }
         return false;
     }
 
-    void tick(){
-        if(this.seconds == 59 && this.minutes == 59 && this.hours == 23){
+    void tick() {
+        if (this.seconds == 59 && this.minutes == 59 && this.hours == 23) {
             this.hours = 0;
             this.minutes = 0;
             this.seconds = 0;
@@ -87,31 +85,96 @@ public class Clock {
         } else if (this.seconds == 59) {
             this.minutes++;
             this.seconds = 0;
-        }
-        else{
+        } else {
             this.seconds++;
         }
     }
 
-    int compare(Clock other){
-        if(this.hours == other.hours && this.minutes == other.minutes && this.seconds == other.seconds){
+    int compare(Clock other) {
+        if (this.hours == other.hours && this.minutes == other.minutes && this.seconds == other.seconds) {
             return 0;
         }
-        if(this.hours > other.hours){
+        if (this.hours > other.hours) {
             return 1;
         }
-        if(this.hours == other.hours && this.minutes > other.minutes){
+        if (this.hours == other.hours && this.minutes > other.minutes) {
             return 1;
         }
-        if(this.minutes == other.minutes && this.seconds > other.seconds){
-             return 1;
+        if (this.minutes == other.minutes && this.seconds > other.seconds) {
+            return 1;
         }
 
         return -1;
     }
 
-    //int secondsDiff(Clock other){
+    int secondsDiff(Clock other) {
+        int absoluteSumHours;
+        int hours = this.hours - other.hours;
+        if (hours < 0) {
+            absoluteSumHours = -hours;
+        } else {
+            absoluteSumHours = hours;
+        }
+        int differenceHours = absoluteSumHours * 3600;
 
-    //}
 
+        int absoluteSumMinutes;
+        int minutes = this.minutes - other.minutes;
+        if (minutes < 0) {
+            absoluteSumMinutes = -minutes;
+        } else {
+            absoluteSumMinutes = minutes;
+        }
+        int differenceMinutes = absoluteSumMinutes * 60;
+
+
+        int absoluteSumSeconds;
+        int seconds = this.seconds - other.seconds;
+        if (seconds < 0) {
+            absoluteSumSeconds = -seconds;
+        } else {
+            absoluteSumSeconds = seconds;
+        }
+        int differenceSeconds = absoluteSumSeconds;
+
+        return differenceHours + differenceMinutes + differenceSeconds;
+    }
+
+    void addSeconds(int toAdd) {
+        this.seconds += toAdd;
+
+        if (this.seconds >= 60) {
+            this.minutes += this.seconds / 60;
+            this.seconds %= 60;
+        }
+        if (this.minutes >= 60) {
+            this.hours += this.minutes / 60;
+            this.minutes %= 60;
+        }
+        if (this.hours >= 24) {
+            this.hours %= 24;
+        }
+        print();
+    }
+
+    void addMinutes(int toAdd) {
+        this.minutes += toAdd;
+        if (this.minutes >= 60) {
+            this.hours += this.minutes / 60;
+            this.minutes %= 60;
+        }
+        if (this.hours >= 24) {
+            this.hours %= 24;
+        }
+        print();
+    }
+
+    void addHours(int toAdd) {
+        this.hours += toAdd;
+
+        if (this.hours >= 24) {
+            this.hours %= 24;
+        }
+        print();
+    }
 }
--- src/termA/lesson8/Main.java
+++ src/termA/lesson8/Main.java
@@ -9,26 +9,33 @@ public class Main {
 
         Clock clock1 = new Clock();
         clock1.hours = 7;
-        clock1.minutes = 7;
-        clock1.seconds = 5;
+        clock1.minutes = 20;
+        clock1.seconds = 10;
 
         //clock.print();
         clock.printAmPm();
 
-        boolean result = clock.isValid();
-        System.out.println(result);
+        //boolean result = clock.isValid();
+        //System.out.println(result);
 
         //clock.randomize();
 
-        boolean isMorning = clock.isMorning();
-        System.out.println(isMorning);
+        //boolean isMorning = clock.isMorning();
+        //System.out.println(isMorning);
 
        // clock.tick();
         //clock.print();
 
-        System.out.println();
-        int compareClocks = clock.compare(clock1);
-        System.out.println(compareClocks);
+        //System.out.println();
+        //int compareClocks = clock.compare(clock1);
+        //System.out.println(compareClocks);
+
+        //int secondsDiff = clock.secondsDiff(clock1);
+        //System.out.println(secondsDiff);
+
+        //clock.addSeconds(20000);
+        //clock.addMinutes(4000);
+        clock.addHours(200);
     }
 
 }
